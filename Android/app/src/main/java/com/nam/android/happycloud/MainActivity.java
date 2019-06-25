2d4f8afc586f9c70d30cd9b4eb61acf00677b5bc
NamZseng <nanrong_z@foxmail.com>
2019-06-26 00:58:33 +0800

    修改欢迎界面，将图片形式的文字，替换成文本文字

--- Android/app/src/main/java/com/nam/android/happycloud/MainActivity.java
+++ Android/app/src/main/java/com/nam/android/happycloud/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 /**
@@ -12,14 +13,14 @@ import android.widget.ImageButton;
  */
 public class MainActivity extends Activity {
 
-    private ImageButton startBtn = null;
+    private Button startBtn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startBtn = (ImageButton) findViewById(R.id.startBtn);
+        startBtn = (Button) findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
