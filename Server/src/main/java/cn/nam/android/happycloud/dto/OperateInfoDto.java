d2444fd6c9865c3787de735e5ec634ac922318b2
NamZseng <nanrong_z@foxmail.com>
2019-07-04 10:07:00 +0800

    完成更改用户昵称操作

---
+++ Server/src/main/java/cn/nam/android/happycloud/dto/OperateInfoDto.java
@@ -0,0 +1,26 @@
+package cn.nam.android.happycloud.dto;
+
+/**
+ * 服务器回传客户端更新删除等操作完成信息
+ *
+ * @author Nanrong Zeng
+ * @version 1.0
+ */
+public class OperateInfoDto {
+
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public OperateInfoDto(String state) {
+        this.state = state;
+    }
+
+
+}
